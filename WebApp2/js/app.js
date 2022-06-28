26bc84f6378ef87758cc341e2886bba66e1f45e6
Tom Perry <tomperry7@gmail.com>
2022-06-28 08:49:51 -0600

    make container class

---
+++ WebApp2/js/app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "images/cat1.jpg" },
+  { name: "Voluptatem", image: "images/cat1.jpg" },
+  { name: "Explicabo", image: "images/cat1.jpg" },
+  { name: "Rchitecto", image: "images/cat1.jpg" },
+  { name: " Beatae", image: "images/cat1.jpg" },
+  { name: " Vitae", image: "images/cat1.jpg" },
+  { name: "Inventore", image: "images/cat1.jpg" },
+  { name: "Veritatis", image: "images/cat1.jpg" },
+  { name: "Accusantium", image: "images/cat1.jpg" },
+]
+
