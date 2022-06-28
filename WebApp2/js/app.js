60d94074de7eae46df0d578639e50d0ce708b504
Tom Perry <tomperry7@gmail.com>
2022-06-28 08:51:26 -0600

    show coffees method

--- WebApp2/js/app.js
+++ WebApp2/js/app.js
@@ -11,3 +11,17 @@ const coffees = [
   { name: "Accusantium", image: "images/cat1.jpg" },
 ]
 
+const showCoffees = () => {
+  let output = ""
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+}
